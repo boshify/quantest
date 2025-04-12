7dec2eb869e1afef9eff1ff9e4836ae090ed990e
Jonathan <me@jonathanboshoff.com>
2025-04-12 12:40:38 -0600

    troubleshooting

--- crypto_strategy/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Use the PORT environment variable if provided, otherwise default to 8000
-PORT=${PORT:-8000}
-
-# Run the application
-exec python -m uvicorn app:app --host 0.0.0.0 --port $PORT 
